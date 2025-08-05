036ff8c914d129a0653116e372ad6a08a6de569b
Kori Miyohashi <Miyohashikori457@Gmail.com>
2025-08-06 00:27:37 +0800

    fix: cors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['storage.sekai.best'],
+    // Or use remotePatterns for more control
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.sekai.best',
+        pathname: '/sekai-jp-assets/**',
+      },
+    ],
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
